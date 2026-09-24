5bb7d6e1a77829f648fd01a1f0a371e106d48667
7adrii <adriloma007@gmail.com>
2026-09-24 18:18:52 +0200

    Implement main program logic to display details of PlatoPrincipal, Bebida, and Postre

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Models;
+
+PlatoPrincipal plato = new PlatoPrincipal("Plato Combinado", 12.50m, "Pollo, patatas, pimientos");
+plato.MostrarDetalles();
+
+Bebida bebida = new Bebida("CocaCola", 8.5m, true);
+bebida.MostrarDetalles();
+
+Postre postre = new Postre("Tarta de queso", 12.45m, 300, false);
+postre.MostrarDetalles();
